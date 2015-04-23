44daef0389dc64ce0949d1cd2a4d9d8f1bd5aafe
Bogdanov Anton <kortirso@gmail.com>
2015-04-23 20:52:18 +0300

    Reconfig erb-files to haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,86 +1,86 @@
 Rails.application.routes.draw do
 
-  post 'positions/create' => 'positions#create', as: 'positions'
+	post 'positions/create' => 'positions#create', as: 'positions'
 
-  resources :measures
-  resources :ingridients
-  resources :line_ingrids
+	resources :measures
+	resources :ingridients
+	resources :line_ingrids
 
-  devise_for :users
-  get 'contacts' => 'contacts#index', as: 'contacts'
-  get 'welcome/index'
+	devise_for :users
+	get 'contacts' => 'contacts#index', as: 'contacts'
+	get 'welcome/index'
 
-  resources :contacts
-  resources :recipes
-  resources :countries
-  resources :categories
-  resources :comments
+	resources :contacts
+	resources :recipes
+	resources :countries
+	resources :categories
+	resources :comments
 
-  get 'carts/:id' => 'carts#show', as: 'cart'
-  delete 'carts/:id' => 'carts#destroy'
+	get 'carts/:id' => 'carts#show', as: 'cart'
+	delete 'carts/:id' => 'carts#destroy'
 
-  get 'catalog/index'
-  get 'catalog/all', as: 'catalog_all'
-  get 'catalog/category/:name' => 'catalog#category', as: 'type'
-  get 'catalog/kitchen/:name' => 'catalog#kitchen', as: 'kitchen'
-  get 'catalog/show/:path_name' => 'catalog#show'
+	get 'catalog/index'
+	get 'catalog/all', as: 'catalog_all'
+	get 'catalog/category/:name' => 'catalog#category', as: 'type'
+	get 'catalog/kitchen/:name' => 'catalog#kitchen', as: 'kitchen'
+	get 'catalog/show/:path_name' => 'catalog#show'
 
-  get 'mark/:mark/recipe/:recipe_id' => 'grades#create', as: 'grading'
+	get 'mark/:mark/recipe/:recipe_id' => 'grades#create', as: 'grading'
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+	# The priority is based upon order of creation: first created -> highest priority.
+	# See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+	# You can have the root of your site routed with "root"
+	# root 'welcome#index'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+	# Example of regular route:
+	#   get 'products/:id' => 'catalog#view'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+	# Example of named route that can be invoked with purchase_url(id: product.id)
+	#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+	# Example resource route (maps HTTP verbs to controller actions automatically):
+	#   resources :products
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+	# Example resource route with options:
+	#   resources :products do
+	#     member do
+	#       get 'short'
+	#       post 'toggle'
+	#     end
+	#
+	#     collection do
+	#       get 'sold'
+	#     end
+	#   end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+	# Example resource route with sub-resources:
+	#   resources :products do
+	#     resources :comments, :sales
+	#     resource :seller
+	#   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+	# Example resource route with more complex sub-resources:
+	#   resources :products do
+	#     resources :comments
+	#     resources :sales do
+	#       get 'recent', on: :collection
+	#     end
+	#   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+	# Example resource route with concerns:
+	#   concern :toggleable do
+	#     post 'toggle'
+	#   end
+	#   resources :posts, concerns: :toggleable
+	#   resources :photos, concerns: :toggleable
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+	# Example resource route within a namespace:
+	#   namespace :admin do
+	#     # Directs /admin/products/* to Admin::ProductsController
+	#     # (app/controllers/admin/products_controller.rb)
+	#     resources :products
+	#   end
 
-  root to: 'catalog#index', as: 'catalog'
+	root to: 'catalog#index', as: 'catalog'
 end
