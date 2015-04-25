7c91fc6c90704d43eb594c4baf24b9a46f361683
Bogdanov Anton <kortirso@gmail.com>
2015-04-25 18:33:33 +0300

    Calculation of all ingredients is added to Busket

--- app/controllers/basket_controller.rb
+++ app/controllers/basket_controller.rb
@@ -5,5 +5,22 @@ class BasketController < ApplicationController
 
 	def show
 		@basket = Basket.new(user_id: current_user.id)
+		@cart.positions.each do |position|
+			count = position.quantity
+			position.recipe.line_ingrids.each do |line|
+				current_ingridient = @basket.products.find_by(ingridient_id: line.ingridient.id)
+					quantity = line.quantity
+					measure = line.measure.id
+					if line.measure.id == 2 or line.measure.id == 4
+						quantity *= 1000
+						measure -= 1
+					end
+					if current_ingridient
+						current_ingridient.quantity += quantity * count
+					else
+						current_ingridient = @basket.products.build(ingridient_id: line.ingridient.id, measure_id: measure, quantity: quantity * count)
+					end
+			end
+		end
 	end
 end
