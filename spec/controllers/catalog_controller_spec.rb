39cf79f2f1b90f998508426fdf08ecec5bb795f5
Bogdanov Anton <kortirso@gmail.com>
2015-06-13 10:40:26 +0300

    add positive specs

--- spec/controllers/catalog_controller_spec.rb
+++ spec/controllers/catalog_controller_spec.rb
@@ -12,18 +12,30 @@ describe CatalogController do
 		end
 	end
 	describe "category action" do
+		it "show recipes from category" do
+			get :category, name: 'soups'
+			expect(response).to render_template :all
+		end
 		it "redirect to 404 when category not exist" do
 			get :category, name: ''
 			expect(response.status).to eq 404
 		end
 	end
 	describe "kitchen action" do
+		it "show recipes from kitchen" do
+			get :kitchen, name: 'spanish'
+			expect(response).to render_template :all
+		end
 		it "redirect to 404 when kitchen not exist" do
 			get :kitchen, name: ''
 			expect(response.status).to eq 404
 		end
 	end
 	describe "show action" do
+		it "show recipe if it exist" do
+			get :show, path_name: 'cheesecake'
+			expect(response).to render_template :show
+		end
 		it "redirect to 404 when recipe not exist" do
 			get :show, path_name: ''
 			expect(response.status).to eq 404
